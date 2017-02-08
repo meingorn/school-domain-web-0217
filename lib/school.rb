c00fd94505cea761e5cec1eb92a3b6fde2c195fa
github username <github email address>
2017-02-08 11:32:24 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -2,7 +2,6 @@ class School
 
   attr_reader :name, :roster
 
-
   def initialize(name, roster={})
     @name = name
     @roster = roster
@@ -16,7 +15,7 @@ class School
   def grade(grade)
     @roster[grade]
   end
-    
+
   def sort
     @roster.map{|grade, arr| arr.sort!}
     @roster
